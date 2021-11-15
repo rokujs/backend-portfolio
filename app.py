0f31eb5ea408544b6fdee087d93a90de3677a763
Sebastian Gil <shiuxcode@gmail.com>
2021-11-14 21:05:04 -0500

    Add end-points skills

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ def get_all_message():
 @app.route('/message', methods=['POST'])
 @cross_origin()
 def create_new_message():
-  print(request.json)
   name = request.json['name']
   email = request.json['email']
   message = request.json['message']
@@ -51,6 +50,76 @@ def create_new_message():
     return response
 
   return 'nana'
+
+# list all skills
+@app.route('/skills', methods=['GET'])
+def get_all_skills():
+  messages = mongo.db.skills.find()
+  response = json_util.dumps(messages)
+  
+  return Response(response, mimetype='application/json')
+
+# create new skill
+@app.route('/skill', methods=['POST'])
+@cross_origin()
+def create_new_skill():
+  name = request.json['name']
+  color = request.json['color']
+  percentage = request.json['percentage']
+  icon = request.json['icon']
+  category = request.json['category']
+
+  skill_id = ""
+  skill_arr = ""
+  skills = list(mongo.db.skills.find())
+
+  for skill in skills:
+    if skill['category'] == category:
+      skill_id = skill['_id']
+      skill_arr = skill['technologies']
+      skill_arr.append({
+          'name': name,
+          'color': color,
+          'percentage': percentage,
+          'icon': icon
+        })
+      break
+
+  if skill_id != "" and name and color and percentage and icon and category:
+    mongo.db.skills.save({
+      '_id': skill_id,
+      'category': category,
+      'technologies': skill_arr
+    })
+
+    response = jsonify({
+      'message': "Create skill",
+    })
+
+    response.status_code = 201
+
+    return response
+  
+  if skill_id == "" and name and color and percentage and icon and category:
+    mongo.db.skills.insert({
+      'category': category,
+      'technologies': [{
+          'name': name,
+          'color': color,
+          'percentage': percentage,
+          'icon': icon
+        }]
+    })
+
+    response = jsonify({
+      'message': "Create skill",
+    })
+
+    response.status_code = 201
+
+    return response
+
+  return 'Error'
 
 @app.errorhandler(400)
 def not_found(error=None):
